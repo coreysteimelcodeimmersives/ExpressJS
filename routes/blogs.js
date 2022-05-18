5ce7e7af151bcf702bd7b9060757f9cd79bac78b
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-05-18 14:04:23 -0600

    finished up hw today

--- routes/blogs.js
+++ routes/blogs.js
@@ -5,23 +5,36 @@ var blogs = require("../public/javascripts/sampleBlogs")
 const blogPosts = blogs.blogPosts
 
 /* GET blogs listing. */
-router.get('/', function(req, res, next) {
-    res.render('blogs', {title: 'Blogs'});
+router.get('/', function (req, res, next) {
+    res.render('blogs', {
+        title: 'Blogs'
+    });
 });
 
 // QUERY PARAM
-router.get('/all', function(req, res){
+router.get('/all', function (req, res) {
     let sort = req.query.sort;
     res.json(sortBlogs(sort));
 });
 
 // ROUTE PARAM
-router.get('/get-by-id/:blogId', (req, res) => {
+router.get('/singleBlog/:blogId', (req, res) => {
     console.log(req.params);
     const blogId = req.params.blogId;
     //JSON: Javascript Object Notation
     res.json(findBlogId(blogId));
-  });
+});
+
+router.get('/postblog', (req, res, next) => {
+    res.render('postBlog');
+});
+
+router.post('/submit', (req, res) => {
+    res.status(201);
+    console.log(req.body);
+    blogPosts.push(addBlogPost(req.body));
+    console.log(blogPosts);
+});
 
 /* HELPER FUNCTION */
 let findBlogId = (id) => {
@@ -29,22 +42,31 @@ let findBlogId = (id) => {
     // ANONYMOUS FUNCTION
     const foundBlog = blogPosts.find(blog => blog.id === id);
     return foundBlog;
-    // for (let i = 0; i < blogPosts.length; i++){
-    //     let blog = blogPosts[i];
-    //     if (blog.id === id){
-    //         return blog;
-    //     }
-    // }
 };
 
 let sortBlogs = (order) => {
-    if (order === 'asc'){
-        return blogPosts.sort(function(a, b){return new Date(a.createdAt) - new Date(b.createdAt)})
-    } else if (order === 'desc'){
-        return blogPosts.sort(function(a,b) {return new Date(b.createdAt) - new Date(a.createdAt)});
+    if (order === 'asc') {
+        return blogPosts.sort(function (a, b) {
+            return new Date(a.createdAt) - new Date(b.createdAt)
+        })
+    } else if (order === 'desc') {
+        return blogPosts.sort(function (a, b) {
+            return new Date(b.createdAt) - new Date(a.createdAt)
+        });
     } else {
         return blogPosts;
     }
+};
+
+let addBlogPost = (body) => {
+    let blog = {
+        createdAt: new Date().toISOString("en-US"),
+        title: body.title,
+        text: body.text,
+        author: body.author,
+        id: String(blogPosts.length + 1)
+    }    
+    return blog;
 }
 
 module.exports = router;
