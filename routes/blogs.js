b323c5aec34e723b3cfcde39da8b082987d2cdd9
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-05-23 14:14:23 -0600

    updating everything pro pro for reference

--- routes/blogs.js
+++ routes/blogs.js
@@ -14,14 +14,15 @@ router.get('/', function (req, res, next) {
 // QUERY PARAM
 router.get('/all', function (req, res) {
     let sort = req.query.sort;
-    res.json(sortBlogs(sort));
+    const sortedBlogs = sortBlogs(sort);
+    res.json(sortedBlogs);
 });
 
 router.get('/display-blogs', function (req, res) {
     res.render('displayBlogs');
 });
 
-router.get('/display-single-blog', (req, res) =>{
+router.get('/display-single-blog', (req, res) => {
     res.render('displaySingleBlog');
 });
 
@@ -29,15 +30,15 @@ router.get('/display-single-blog', (req, res) =>{
 router.get('/single-blog/:blogId', (req, res) => {
     console.log(req.params);
     const blogId = req.params.blogId;
-    //JSON: Javascript Object Notation
-    res.json(findBlogId(blogId));
+    const foundBlog = findBlogId(blogId)
+    res.json(foundBlog);
 });
 
 router.delete('/delete-blog/:blogId', (req, res) => {
     const blogId = req.params.blogId;
-    deleteBlogById(blogId)
+    const filteredBlogList = generateBlogs(blogPosts, blogId, "filter");
+    saveBlogPosts(filteredBlogList);
     res.json('Successfully Deleted');
-    console.log(blogPosts);
 });
 
 router.get('/post-blog', (req, res, next) => {
@@ -45,23 +46,61 @@ router.get('/post-blog', (req, res, next) => {
 });
 
 router.post('/submit', (req, res) => {
-    console.log(req.body);
     blogPosts.push(addBlogPost(req.body));
-    console.log(blogPosts);
+    res.json('OK')
 });
 
+router.put('/update-blog/:blogId', (req, res) => {
+    console.log('hit route')
+    const blogId = req.params.blogId;
+    const title = req.body.title;
+    const text = req.body.text;
+    const author = req.body.author;
+    const updatedBlogData = {
+        title,
+        text,
+        author
+    }
+    const updatedBlogList = generateBlogs(blogPosts, blogId, "update", updatedBlogData);
+    saveBlogPosts(updatedBlogList);
+    res.json('OK');
+})
+
 /* HELPER FUNCTION */
-let deleteBlogById = (id) => {
-    console.log('id')
-    console.log(id)
-    blogPosts = blogPosts.filter(blog => blog.id != id);
-    console.log('remaining blogs')
-    console.log(blogPosts)
-    return blogPosts;
+let saveBlogPosts = (blogList) => {
+    return blogPosts = blogList;
+}
+
+let generateBlogs = (blogList, blogId, filterOrUpdate, updatedBlogData) => {
+
+    if (!blogId) {
+        return blogList;
+    }
+
+    if (filterOrUpdate === "filter") {
+        return blogList.filter((blog) => blog.id != blogId);
+    }
+
+    if (filterOrUpdate === "update") {
+        return blogList.map((blog) => {
+            if (blog.id === blogId) {
+                return {
+                    ...blog,
+                    title: updatedBlogData.title ? updatedBlogData.title : blog.title,
+                    text: updatedBlogData.text ? updatedBlogData.text : blog.text,
+                    author: updatedBlogData.author ? updatedBlogData.author : blog.author
+                }
+            }
+            return blog;
+        })
+    }
+}
+
+let filterBlogListById = (bloglist, id) => {
+    return bloglist.filter(blog => blog.id != id);;
 }
 
 let findBlogId = (id) => {
-
     // ANONYMOUS FUNCTION
     const foundBlog = blogPosts.find(blog => blog.id === id);
     return foundBlog;
@@ -88,7 +127,7 @@ let addBlogPost = (body) => {
         text: body.text,
         author: body.author,
         id: String(blogPosts.length + 1)
-    }    
+    }
     return blog;
 }
 
