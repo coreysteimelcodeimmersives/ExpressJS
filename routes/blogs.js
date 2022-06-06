7922f58f92e3897b0f466497a22966ea59a61719
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-06-06 11:02:11 -0600

    monday morning progress

--- routes/blogs.js
+++ routes/blogs.js
@@ -55,14 +55,23 @@ router.get('/display-single-blog', (req, res) => {
 // ROUTE PARAM
 router.get('/single-blog/:blogId', async function (req, res) {
     try {
-        const blogId = Number(req.params.blogId) - 1;
+        const blogId = Number(req.params.blogId);
         const collection = await blogsDB().collection('posts50');
-        // const foundBlog = await collection.findOne({
-        //     id: blogId
-        // });
-        const blogs = await collection.find({}).toArray();
-        const foundBlog = blogs[blogId];
-        res.json(foundBlog);
+        const foundBlog = await collection.findOne({
+            id: blogId
+        });
+        if (!foundBlog) {
+            const noBlog = {
+                title: "",
+                text: "This Blog does not exist",
+                author: '',
+                id: ""
+            }
+            res.json(noBlog)
+        } else {
+            res.json(foundBlog);
+        }
+
     } catch (error) {
         res.status(500).send("Error fetching posts." + error)
     }
@@ -73,8 +82,9 @@ router.delete('/delete-blog/:blogId', async (req, res) => {
     try {
         const blogId = Number(req.params.blogId);
         const collection = await blogsDB().collection('posts50');
-        const blogToDelete = await collection.findOne({id: blogId});
-        await collection.deleteOne({id: blogId})
+        await collection.deleteOne({
+            id: blogId
+        })
         res.status(200).send('Successfully Deleted')
     } catch (error) {
         res.status(500).send("Error deleting blog." + error)
@@ -88,26 +98,31 @@ router.get('/post-blog', (req, res, next) => {
 router.post('/submit', async function (req, res) {
     try {
         const collection = await blogsDB().collection('posts50');
-        const sortedBlogArr = await collection.find({}).sort({id: 1}).toArray();
+        const sortedBlogArr = await collection.find({}).sort({
+            id: 1
+        }).toArray();
         const lastBlog = sortedBlogArr[sortedBlogArr.length - 1];
-        let blog = req.body;
-        const blogTitle = blog.title ? blog.title : '';
-        const blogText = blog.text ? blog.text : '';
-        const blogAuthor = blog.author ? blog.author : '';
-        const blogCategory = blog.category ? blog.category : '';
-        const today = new Date();
-        const newBlog = {
-            createdAt: today,
-            lastModified: today,
-            title: blogTitle,
-            text: blogText,
-            author: blogAuthor,
-            category: blogCategory,
-            id: Number(lastBlog.id + 1),
-        };
 
-        await collection.insertOne(newBlog);
-        res.status(200).send('Successfully Posted')
+        let blog = req.body;
+
+        if (!blog.title || !blog.text || !blog.author || !blog.category) {
+            console.log("if block")
+            let mssg = "To submit a blog please include:<br>"
+            mssg = !blog.title ? mssg += "- a title<br>" : '';
+            mssg = !blog.text ? mssg += "- blog text<br> " : '';
+            mssg = !blog.author ? mssg += "- an author<br> " : '';
+            mssg = !blog.category ? mssg += "- a category<br> " : '';
+            res.send(mssg);
+        } else {
+            blog = {
+                createdAt: new Date(),
+                lastModified: new Date(),
+                id: Number(lastBlog.id + 1),
+            }
+            await collection.insertOne(blog);
+            res.status(200).send('Successfully Posted')
+        }
+
     } catch (error) {
         res.status(500).send("Error posting blog." + error)
     }
@@ -118,69 +133,75 @@ router.put('/update-blog/:blogId', async function (req, res) {
     try {
         const collection = await blogsDB().collection('posts50');
         const blogId = Number(req.params.blogId);
-        const ogBlog = await collection.findOne({id: blogId});
-        let updateBlog = req.body;
-        const blogTitle = updateBlog.title ? updateBlog.title : ogBlog.title;
-        const blogText = updateBlog.text ? updateBlog.text : ogBlog.text;
-        const blogAuthor = updateBlog.author ? updateBlog.author : ogBlog.author;
-        const blogCategory = updateBlog.category ? updateBlog.category : ogBlog.category;
-        updateBlog = {
-            lastModified: new Date(),
-            title: blogTitle,
-            text: blogText,
-            author: blogAuthor,
-            category: blogCategory,
-        };
-        await collection.updateOne({
+        const ogBlog = await collection.findOne({
             id: blogId
-        }, {
-            $set: updateBlog
         });
-        res.status(200).send('Successfully Updated')
+        if (!ogBlog) {
+            res.send('Blog Id: ' + blogId + " does not exist.")
+        } else {
+            let updateBlog = req.body;
+            const blogTitle = updateBlog.title ? updateBlog.title : ogBlog.title;
+            const blogText = updateBlog.text ? updateBlog.text : ogBlog.text;
+            const blogAuthor = updateBlog.author ? updateBlog.author : ogBlog.author;
+            const blogCategory = updateBlog.category ? updateBlog.category : ogBlog.category;
+            updateBlog = {
+                lastModified: new Date(),
+                title: blogTitle,
+                text: blogText,
+                author: blogAuthor,
+                category: blogCategory,
+            };
+            await collection.updateOne({
+                id: blogId
+            }, {
+                $set: updateBlog
+            });
+            res.status(200).send('Successfully Updated Blog Id: ' + blogId)
+        }
     } catch (error) {
         res.status(500).send("Error updating blog." + error)
     }
 })
 
 /* HELPER FUNCTION */
-let saveBlogPosts = (blogList) => {
-    return blogPosts = blogList;
-}
+// let saveBlogPosts = (blogList) => {
+//     return blogPosts = blogList;
+// }
 
-let generateBlogs = (blogList, blogId, filterOrUpdate, updatedBlogData) => {
+// let generateBlogs = (blogList, blogId, filterOrUpdate, updatedBlogData) => {
 
-    if (!blogId) {
-        return blogList;
-    }
+//     if (!blogId) {
+//         return blogList;
+//     }
 
-    if (filterOrUpdate === "filter") {
-        return blogList.filter((blog) => blog.id != blogId);
-    }
+//     if (filterOrUpdate === "filter") {
+//         return blogList.filter((blog) => blog.id != blogId);
+//     }
 
-    if (filterOrUpdate === "update") {
-        return blogList.map((blog) => {
-            if (blog.id === blogId) {
-                return {
-                    ...blog,
-                    title: updatedBlogData.title ? updatedBlogData.title : blog.title,
-                    text: updatedBlogData.text ? updatedBlogData.text : blog.text,
-                    author: updatedBlogData.author ? updatedBlogData.author : blog.author
-                }
-            }
-            return blog;
-        })
-    }
-}
+//     if (filterOrUpdate === "update") {
+//         return blogList.map((blog) => {
+//             if (blog.id === blogId) {
+//                 return {
+//                     ...blog,
+//                     title: updatedBlogData.title ? updatedBlogData.title : blog.title,
+//                     text: updatedBlogData.text ? updatedBlogData.text : blog.text,
+//                     author: updatedBlogData.author ? updatedBlogData.author : blog.author
+//                 }
+//             }
+//             return blog;
+//         })
+//     }
+// }
 
-let filterBlogListById = (bloglist, id) => {
-    return bloglist.filter(blog => blog.id != id);;
-}
+// let filterBlogListById = (bloglist, id) => {
+//     return bloglist.filter(blog => blog.id != id);;
+// }
 
-let findBlogId = (id) => {
-    // ANONYMOUS FUNCTION
-    const foundBlog = blogPosts.find(blog => blog.id === id);
-    return foundBlog;
-};
+// let findBlogId = (id) => {
+//     // ANONYMOUS FUNCTION
+//     const foundBlog = blogPosts.find(blog => blog.id === id);
+//     return foundBlog;
+// };
 
 // let sortBlogs = (order, posts) => {
 //     if (order === 'asc') {
@@ -196,15 +217,15 @@ let findBlogId = (id) => {
 //     }
 // };
 
-let addBlogPost = (body) => {
-    let blog = {
-        createdAt: new Date().toISOString("en-US"),
-        title: body.title,
-        text: body.text,
-        author: body.author,
-        id: String(blogPosts.length + 1)
-    }
-    return blog;
-}
+// let addBlogPost = (body) => {
+//     let blog = {
+//         createdAt: new Date().toISOString("en-US"),
+//         title: body.title,
+//         text: body.text,
+//         author: body.author,
+//         id: String(blogPosts.length + 1)
+//     }
+//     return blog;
+// }
 
 module.exports = router;
